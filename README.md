cfd68285f89ebcf18a0c50e53d6b951e192fa722
Artem <example.com>
2021-08-03 12:06:39 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # screen-resolution-setter
+Set resolution on VM
---
+++ index.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+module.exports.setResolution = function(width, height) {
+	return new Promise((resolve, reject) => {
+
+		if (!width) {
+			reject(new Error("Resolution width is required"));
+		}
+
+		if (!height) {
+			reject(new Error("Resolution height is required"));
+		}
+
+		exec(`python ${__dirname}\\resolution.py ${width} ${height}`, {windowsHide: true}, (error, stdout, stderr) => {
+			if (error) {
+				reject(error);
+			}
+
+			resolve();
+		});
+	});
+};
