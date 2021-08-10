11833aeac43799a5d7cff3c28990509d0cedb701
Sergei Krivosheenko <serkyron@gmail.com>
2021-08-10 17:49:56 +0300

    support only win32, exeFile added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { exec } = require('child_process');
+const { execFile } = require('child_process');
+const os = require('os');
 
 module.exports.setResolution = function(width, height) {
     return new Promise((resolve, reject) => {
+        if (os.platform() !== "win32") {
+            reject(new Error("Platform not supported"));
+        }
+
 
         if (!width) {
             reject(new Error("Resolution width is required"));
